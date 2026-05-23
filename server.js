d9b169e09ef659c19133fba74c4f9fb66a7c9ceb
Heyyyakashhhh <heyyyakash907@gmail.com>
2026-05-23 09:05:02 +0530

    change port no.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // मोंगोडीबी कनेक्शन
 mongoose
